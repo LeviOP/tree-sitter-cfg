37950bf88ca43d9f5af4c4782b053d3560168b45
Levi Frandsen <l3viop@gmail.com>
2024-02-12 14:33:09 -0600

    add comma to characters that breaks tokens, ignore more than space

--- grammar.js
+++ grammar.js
@@ -2,7 +2,7 @@ module.exports = grammar({
     name: "cfg",
 
     extras: $ => [
-        " "
+        [\x00-\x20]
     ],
 
     rules: {
@@ -21,11 +21,11 @@ module.exports = grammar({
         _quoted_token: $ => seq("\"", $.quoted_token, token.immediate(optional("\""))),
         quoted_token: $ => /[^\n"]*/,
         unquoted_token: $ => choice(
-            //All characters in `g_BreakSetIncludingColons` become their own tokens if unquoted
-            /[{}()':]/,
+            //Characters that always become their own tokens
+            /[{}()':,]/,
             //Unquoted token that can contain quote characters. After a quote, a semicolon may occur without ending the token. Subsequent quotes disallow semicolons, and then allow, and so on.
-            /[^\n {}()':";]+("[^\n {}()':"]*"[^\n {}()':";]*)*("[^\n {}()':"]*)?/,
-            //Comment, which is technically just a token that gets ignored by COM_parse, but is still included in cmd_args
+            /[^\n {}()':,";]+("[^\n {}()':,"]*"[^\n {}()':,";]*)*("[^\n {}()':,"]*)?/,
+            //Comment, which is technically just a token which isn't added to cmd_argv by COM_Parse, but is still included in cmd_args
             /\/\/[^;\n]*/
         )
     }
